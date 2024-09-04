321f7a944051681d9cccabda9a87826e83db1178
HELIOPOTELICKI <helio.p2000@hotmail.com>
2024-09-04 15:49:48 -0300

    refactor [Chatwoot Integration]: Update references from Chatwoot to CogmoCRM
    This commit modifies the references from 'Chatwoot' to 'CogmoCRM' for better alignment to the adopted CRM in the provided widgets. After these changes, users will interact with actions labelled as 'CogmoCRM' instead of 'Chatwoot'. Additionally, the base URL for the service is updated to reflect the new CRM destination.

--- packages/schemas/features/blocks/integrations/chatwoot/constants.ts
+++ packages/schemas/features/blocks/integrations/chatwoot/constants.ts
@@ -1,8 +1,8 @@
 import { ChatwootBlock } from './schema'
 
-export const chatwootTasks = ['Show widget', 'Close widget'] as const
+export const chatwootTasks = ['Show CogmoCRM', 'Close CogmoCRM'] as const
 
 export const defaultChatwootOptions = {
-  task: 'Show widget',
-  baseUrl: 'https://app.chatwoot.com',
+  task: 'Show CogmoCRM',
+  baseUrl: 'https://cogmocrm.novo.org.br',
 } as const satisfies ChatwootBlock['options']
